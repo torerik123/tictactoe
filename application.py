79a5854b0c2d7352de1a58bb82c893126011cc0b
torerik123 <tor_erik_grimen@hotmail.com>
2020-12-04 10:17:16 +0100

    heroku

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ import os
 
 app = Flask(__name__)
 
-app.config["SESSION_FILE_DIR"] = mkdtemp()
+#app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
